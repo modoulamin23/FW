d42324901f91842cb431a898432754b874c36e33
modoulamin23 <83877569+modoulamin23@users.noreply.github.com>
2021-05-08 17:01:56 +0000

    CV Website

---
+++ script.js
@@ -0,0 +1,18 @@
+//alert("Hello!");
+//alert("welcome!");
+//let firstName = "Modou Lamin";
+//let lastName = "Jagne";
+//let myName = firstName + " " + lastName;
+//console.log("My name is " + myName)
+let country = "Gambia";
+let continent = "Africa";
+let population = "2.2 million";
+console.log (country);
+console.log (continent);
+console.log (population); 
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
